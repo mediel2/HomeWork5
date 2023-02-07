69042b7cb976c7d83d72943274a5868e16f9623e
Denis Tulupov <med1el@mail.ru>
2023-02-07 16:59:26 +0300

    tasks

---
+++ 1. Регулярное выражение.js
@@ -0,0 +1,5 @@
+let str_1 =  'ahb acb aeb aeeb adcb axeb';
+
+let rezult = str_1.match(/a\Db/g);
+
+console.log(rezult);
---
+++ 2. Регулярное выражение 2.js
@@ -0,0 +1,5 @@
+let str_1 =  '2+3 223 2223';
+
+let rezult = str_1.match(/2\+3/g);
+
+console.log(rezult);
